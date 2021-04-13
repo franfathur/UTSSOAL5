9a73f66f37aa6121e4bc9b5af29ba90a84100baa
franfathur <82429851+franfathur@users.noreply.github.com>
2021-04-13 21:35:52 +0700

    Add files via upload

---
+++ PreferredCustomerDemonstrate.java
@@ -0,0 +1,6 @@
+public class PreferredCustomerDemonstrate {
+    public static void main(String[] args){
+        PreferredCustomer preferredCustomer = new PreferredCustomer("Ghufran Fathur Rahman", "Brenggolo Kediri", "0024-0024", "024", true , 1000);
+        System.out.println(preferredCustomer.toString());
+    }
+}
